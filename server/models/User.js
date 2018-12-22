962bc09a47813611e5d8ce6ac07d559c63ef66d8
Oxyrus <Oxyrus@users.noreply.github.com>
2018-12-21 20:53:33 -0500

    Add relationship to communities

--- server/models/User.js
+++ server/models/User.js
@@ -1,40 +1,15 @@
-const bookshelf = require('../db');
+const { bookshelf } = require('../db');
 
 const User = bookshelf.Model.extend({
   tableName: 'users',
   hasTimestamps: false,
+  communities() {
+    return this.belongsToMany('Community')
+  }
 }, {
   byDisplayName(displayName) {
     return this.forge().query({ where: { display_name: displayName } }).fetch()
   }
 })
 
-const Users = bookshelf.Collection.extend({  
-  model: User
-})
-
-module.exports = {
-  User,
-  Users,
-}
-
-/**
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const userSchema = new Schema({
-  facebookId: String,
-  googleId: String,
-  displayName: String,
-  familyName: String,
-  givenName: String,
-  photo: String,
-  bio: String,
-  email: String,
-  location: String,
-  dateCreated: { type: Date, default: Date.now() },
-  lastUpdated: { type: Date }
-})
-
-module.exports = mongoose.model('communities-user', userSchema)
-*/
+module.exports = bookshelf.model('User', User)
